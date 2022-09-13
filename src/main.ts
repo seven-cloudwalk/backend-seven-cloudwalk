b010e03f9aa0f66a44c2dfbb24edf9549ad758ff
Jaymeson Mendes <jaymesonmendes@gmail.com>
2022-09-12 22:08:16 -0300

    feature: criado o resource de products

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const PORT =process.env.PORT || 3500;
+const PORT = process.env.PORT || 3500;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
