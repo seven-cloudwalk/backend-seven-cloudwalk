f94fada45fc1d21b87dbfdf46de6a4575df8db27
hefi1413 <hefi1413@gmail.com>
2022-09-12 21:22:58 -0300

    feature: configuração do banco e auth
    - configuração do .env
    - criação do folfer \auth com verificação do usuário e usuário logado
    - criação do folder \users para utilização nos testes de autenticação

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,34 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly prisma: PrismaService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: { email: string, cpf: string, isAdmin: Boolean }) {
+
+    const user = await this.prisma.users.findUnique({
+      where: { email: payload.email },
+    });
+
+    //console.log('user:', user );
+
+    if (!user) {
+      throw new UnauthorizedException(
+        'Usuário não existe ou não está autenticado',
+      );
+    }
+
+    delete user.password;
+
+    return user;
+  }
+}
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,10 @@
+export class Users {
+    id?:       string;
+    nickname:  string;
+    email:     string;
+    password:  string;
+    pj:        boolean;
+    role:   boolean;
+    createdAt?: Date;
+    updatedAt?: Date;
+ }
