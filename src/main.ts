fbccbde40265a8466ca39de52e659213f35a255a
Jaymeson Mendes <jaymesonmendes@gmail.com>
2022-09-14 20:12:21 -0300

    feature: ajustado o controller do modulo de produtos

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
-const PORT = process.env.PORT || 3500;
-
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(PORT);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('PizzaFresh')
+    .setDescription('Application for managing orders for a pizzeria... 😋')
+    .setVersion('1.0.0')
+    .addTag('auth')
+    .addTag('users')
+    .addTag('products')
+    .addTag('favorites')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
