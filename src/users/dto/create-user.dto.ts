00fd81397033340fa7b2cf34366a66f80a0c2e8f
Jaymeson Mendes <jaymesonmendes@gmail.com>
2022-09-19 20:29:53 -0300

    feature: atualizações

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -2,39 +2,38 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsBoolean } from 'class-validator';
 
 export class CreateUserDto {
-    @IsString()
-    @ApiProperty({
-      description: 'Apelido do usuário',
-      example: 'nome um'
-    })
-    nickname: string;
+  @IsString()
+  @ApiProperty({
+    description: 'Apelido do usuário',
+    example: 'nome um',
+  })
+  nickname: string;
 
-    @IsString()
-    @ApiProperty({
-      description: 'Email do usuário',
-      example: 'nome1@gmail.com'
-    })
-    email: string;
+  @IsString()
+  @ApiProperty({
+    description: 'Email do usuário',
+    example: 'nome1@gmail.com',
+  })
+  email: string;
 
-    @IsString()
-    @ApiProperty({
-      description: 'Senha do usuário',
-      example: 'teste'
-    })
-    password:  string;
+  @IsString()
+  @ApiProperty({
+    description: 'Senha do usuário',
+    example: 'teste',
+  })
+  password: string;
 
-    @IsString()
-    @ApiProperty({
-      description: 'informa se este usuário é uma pessoa jurídica',
-      example: 'true'
-    })
-    pj:   boolean;
-    
-    @IsBoolean()
-    @ApiProperty({
-      description: 'Informa se este usuário é um administrador',
-      example: 'false'
-    })
-    role:  boolean;
+  @IsString()
+  @ApiProperty({
+    description: 'informa se este usuário é uma pessoa jurídica',
+    example: 'true',
+  })
+  pj: boolean;
+
+  @IsBoolean()
+  @ApiProperty({
+    description: 'Informa se este usuário é um administrador',
+    example: 'false',
+  })
+  role: boolean;
 }
-
