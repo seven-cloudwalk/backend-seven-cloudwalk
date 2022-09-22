b93668becf30454be01cf374726d1102ea529011
Jaymeson Mendes <jaymesonmendes@gmail.com>
2022-09-22 15:10:38 -0300

    feature: criado a rota POST de favoritos

--- src/favorite/favorite.controller.ts
+++ src/favorite/favorite.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { FavoriteService } from './favorite.service';
+import { FavoriteProductDto } from './dto/favorite.dto';
+import { Favorite } from './entities/favorite.entity';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('favorites')
 export class FavoriteController {
   constructor(private readonly favoriteService: FavoriteService) {}
+
+  @Post('favorite')
+  @ApiOperation({
+    summary: 'Favoritar um produto',
+  })
+  favoriteProduct(@Body() dto: FavoriteProductDto): Promise<Favorite> {
+    return this.favoriteService.favoriteProduct(dto);
+  }
 }
--- src/favorite/favorite.service.ts
+++ src/favorite/favorite.service.ts
@@ -1,78 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from './../prisma/prisma.service';
+import { FavoriteProductDto } from './dto/favorite.dto';
+import { Favorite } from './entities/favorite.entity';
 
 @Injectable()
 export class FavoriteService {
   constructor(private readonly prisma: PrismaService) {}
+
+  favoriteProduct(dto: FavoriteProductDto): Promise<Favorite> {
+    return this.prisma.favorite.create({ data: dto });
+  }
 }
-
-// async favoriteProduct(dto: FavoriteProductDto): Promise<Favorite> {
-//     await this.verifyUserId(dto.iduser);
-
-//     const product: Product = await this.prisma.product.findUnique({
-//       where: { name: dto.idproduct },
-//     });
-
-//     if (!product) {
-//       throw new NotFoundException(
-//         `Produto de nome '${dto.idproduct}' não encontrado`,
-//       );
-//     }
-
-//     const data: Prisma.FavoriteCreateInput = {
-//       iduser: {
-//         connect: {
-//           id: dto.iduser,
-//         },
-//       },
-//       idproduct: {
-//         connect: {
-//           name: dto.idproduct,
-//         },
-//       },
-//     };
-
-//     return this.prisma.favorite
-//       .create({ data })
-//       .catch(handleErrorConstraintUnique);
-//   }
-
-//   async unfavoriteProduct(id: string) {
-//     const favorite: Favorite = await this.prisma.favorite.findUnique({
-//       where: { id },
-//     });
-
-//     if (!favorite) {
-//       throw new NotFoundException(`Entrada de id '${id}' não encontrada`);
-//     }
-
-//     return this.prisma.favorite.delete({ where: { id } });
-//   }
-
-//   async getUserFavorites(id: string): Promise<Favorite[]> {
-//     await this.verifyUserId(id);
-
-//     return this.prisma.favorite.findMany({ where: { userId: id } });
-//   }
-
-//   async getUsersWhoFavoritedProduct(id: string) {
-//     const product: Product = await this.prisma.product.findUnique({
-//       where: { id },
-//     });
-
-//     if (!product) {
-//       throw new NotFoundException(`Entrada de id '${id}' não encontrada`);
-//     }
-
-//     return this.prisma.favorite.findMany({ where: { product: { id } } });
-//   }
-
-//   async verifyUserId(id: string): Promise<void | never> {
-//     const user: User = await this.prisma.user.findUnique({
-//       where: { id },
-//     });
-
-//     if (!user) {
-//       throw new NotFoundException(`Entrada de id '${id}' não encontrada`);
-//     }
-//   }
